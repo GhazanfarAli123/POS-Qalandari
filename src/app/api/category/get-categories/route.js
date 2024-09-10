d4a755423005f56459df484cf1305e698a7fc92c
GhazanfarAli123 <84629081+GhazanfarAli123@users.noreply.github.com>
2024-09-11 00:16:20 +0500

    changes

--- src/app/api/category/get-categories/route.js
+++ src/app/api/category/get-categories/route.js
@@ -3,12 +3,16 @@ import dbConnect from '@utils/dbconnect'
 import Category from '@models/Category'
 
 export async function GET(req, res) {
-  dbConnect()
-
+  await dbConnect()
   try {
     const category = await Category.find({})
-
-    return new Response(JSON.stringify({ message: 'Get all categories successfully', category }), { status: 200 })
+    return new Response(JSON.stringify({ message: 'Get all categories successfully', category }), {
+      status: 200,
+      headers: {
+        'Cache-Control': 'no-store', // Prevents Vercel caching
+        'Content-Type': 'application/json'
+      }
+    })
   } catch (error) {
     console.error(error)
     return new Response(JSON.stringify({ message: 'Server error' }), { status: 500 })
